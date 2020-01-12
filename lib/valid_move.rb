bc004b06cbcd281ae0df07075987d3aeb32bdeae
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-01-12 15:44:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,14 @@
 # code your #valid_move? method here
-def valid_move
+def valid_move(array, index)
+  board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
   if position_taken == true
     return true
   else
     return false
   end
 end
+
+valid_move(board, board[1])
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken(array, index)
